fc2d539b16da4e5a3ab84dfb349c3c3dbc70cf6b
aman adinlid <95pinkpanda@gmail.com>
2021-04-17 13:56:39 +0200

    ##Added validation to entity classes##

--- src/main/java/lexicon/se/practice/entity/Customer.java
+++ src/main/java/lexicon/se/practice/entity/Customer.java
@@ -1,7 +1,40 @@
 package lexicon.se.practice.entity;
 
-import javax.persistence.Entity;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
 @Entity
 public class Customer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(nullable = false, updatable = false)
+    @NotNull
+    private String customerId;
+
+
+    @Size( max =80 ,message = "should be a well-formed email.")
+    @Column(nullable = false, length = 80)
+    @NotNull
+    private String email;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "TIMESTAMP default CURRENT_TIMESTAMP")
+    @NotNull
+    private LocalDate regDate;
+
+    @Column(nullable = false, columnDefinition = "tinyint(1) default 1")
+    private boolean active;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @NotNull
+    private CustomerDetails details;
+
 }
--- src/main/java/lexicon/se/practice/entity/CustomerDetails.java
+++ src/main/java/lexicon/se/practice/entity/CustomerDetails.java
@@ -1,5 +1,43 @@
 package lexicon.se.practice.entity;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
 public class CustomerDetails {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String detailsId;
+
+    @NotNull(message = "Name should not be null")
+    @Size(min = 2, max = 4, message = "Must be between 2 and 4")
+    @Column(nullable = false, length = 4)
+    private String street;
+
+    @NotNull
+    @Size(max = 5, message = "should be 5 numbers")
+    @Column(nullable = false, length = 5)
+    private String zipCode;//35263
+
+    @NotNull
+    @Column(nullable = false, length = 20)
+    private String city;
+
+    @NotNull
+    @Column(nullable = false, length = 10)
+    private String homePhone;
+
+    @NotNull
+    @Column(nullable = false, length = 10)
+    private String cellPhone;
 }
